f6e78346849c1dc5c8c5ffdefd55fc91c13614b5
Jadson Butzk <84212592+JadsonButzk@users.noreply.github.com>
2021-12-12 19:13:21 -0300

    Add files via upload

---
+++ script_desafio.js
@@ -0,0 +1,5 @@
+function dadosdolead() {
+    var nome=document.getElementById("nome")
+    var email=document.getElementById("email")
+    alert(`Tudo certo! lhe enviaremos um email posteriormente`)
+}
