1fc208799e4d4accc52cddd0aac9547a00ba60b3
Jadson Butzk <84212592+JadsonButzk@users.noreply.github.com>
2021-12-12 19:26:12 -0300

    Delete script_desafio.js

--- script_desafio.js
+++
@@ -1,5 +0,0 @@
-function dadosdolead() {
-    var nome=document.getElementById("nome")
-    var email=document.getElementById("email")
-    alert(`Tudo certo! lhe enviaremos um email posteriormente`)
-}
